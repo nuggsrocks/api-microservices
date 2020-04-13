d98f7f8ded340fe1f344c184220a0ee6a50ad85a
mattfisher <nuggsrocks@yahoo.com>
2020-04-13 18:59:38 -0400

    added own request logging middleware

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const requestLogMiddleware = require('request-logging-module');
 
 const port = 8080;
 
 app.listen(port, () => console.log('server is running at http://localhost:' + port + '...'));
+
+app.use((req, res, next) => requestLogMiddleware(req, res, next));
 
 app.get('/', (req, res) => res.send('Hello World'));

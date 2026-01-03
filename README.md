390ef8e0d10c0e9e9fbf3be260fa34c31c6db19a
mattfisher <nuggsrocks@yahoo.com>
2026-01-02 23:57:14 -0500

    update dependencies

--- README.md
+++ README.md
@@ -7,8 +7,11 @@ https://nuggsrocks-api-microservices.glitch.me/
 ### /api/timestamp/:date_string?
 accepts optional utc format date string and returns json with unix and utc time values.
 
+
 ### /api/whoami
 returns json with request header ip, language, and user system info
 
+
+#### DEPRECATED
 ### /api/wiki-search?search=<search+string>
 redirects to a wikipedia page matching the query string, if one exists
--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 const express = require("express");
-const fetch = require("node-fetch");
 
 const app = express();
 
 const port = 3000;
 
 app.listen(port, () =>
-  console.log("server is running at http://localhost:" + port + "...")
+  console.log("server is running at http://localhost:" + port)
 );
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => res.sendFile(__dirname + "/README.md"));
+app.get("/", (_, res) => res.sendFile(__dirname + "/README.md"));
 
-app.get("/api/timestamp/:date_string?", (req, res) => {
-  let input = req.params.date_string;
+app.get("/api/timestamp{/:date_string}", (req, res) => {
+  const input = req.params.date_string;
   let date;
   if (input === undefined) {
     date = new Date();
@@ -26,7 +25,7 @@ app.get("/api/timestamp/:date_string?", (req, res) => {
   if (date.toString().search("Invalid Date") !== -1) {
     res.json({ error: date.toString() });
   } else {
-    let body = {
+    const body = {
       unix: Date.parse(date.toUTCString()),
       utc: date.toUTCString(),
     };
@@ -35,23 +34,13 @@ app.get("/api/timestamp/:date_string?", (req, res) => {
 });
 
 app.get("/api/whoami", (req, res) => {
-  let ip = req.ip;
-  let lang = req.get("Accept-Language");
-  let software = req.get("User-Agent");
-  let body = {
+  const ip = req.ip;
+  const lang = req.get("Accept-Language");
+  const software = req.get("User-Agent");
+  const body = {
     ipaddress: ip,
     language: lang,
     software: software,
   };
   res.json(body);
-});
-
-app.get("/api/wiki-search/", (req, res) => {
-  let search = req.query.search;
-  fetch("https://en.wikipedia.org/w/api.php?action=opensearch&search=" + search)
-    .then((res) => res.json())
-    .then((data) => {
-      res.redirect(data[3][0]);
-    })
-    .catch((e) => res.send(e));
 });
